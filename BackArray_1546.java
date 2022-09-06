9a24bb6a298c9e75f9caccf88097d5df41a3a113
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-06 16:41:10 +0900

    Add files via upload

---
+++ BackArray_1546.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BackArray_1546  {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+        int sum = 0;
+        double avg = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            
+        	arr[i] = sc.nextInt();
+            
+        	if(arr[i] > max) {
+                max = arr[i];
+            }
+            sum += arr[i];
+        }
+        
+        sc.close();
+
+        avg = 100.0 *sum/max/n;
+        System.out.println(avg);
+
+
+    }
+}

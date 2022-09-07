c6fe1b653b20c50b38df3b167d630c08b8a606bf
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-07 16:34:16 +0900

    Add files via upload

---
+++ BackMath01_1712.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BackMath01_1712 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int result = 0; 
+
+		if(C-B<=0) {	
+			result = -1;
+			System.out.println(result);
+		}
+		else {
+			result = A/(C-B);	//손익분기점시 개수
+			System.out.println(result+1);
+		}
+		
+	}
+
+}

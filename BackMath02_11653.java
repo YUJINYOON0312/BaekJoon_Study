2c5dd9690e932c502c1290c7d3af5561808d2ced
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-13 13:55:20 +0900

    Add files via upload

---
+++ BackMath02_11653.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BackMath02_11653 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=2; i<10000000; i++) {
+			
+			while(n%i==0) {
+				System.out.println(i);
+				n = (n/i);
+			}
+		}
+
+	}
+
+}

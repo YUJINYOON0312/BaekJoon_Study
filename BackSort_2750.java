30ac8f14889dcf68c423b74f953108517a023743
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2022-09-17 17:27:41 +0900

    Update BackSort_2750.java

--- BackSort_2750.java
+++ BackSort_2750.java
@@ -17,9 +17,9 @@ public class BackSort_2750 {
 		for(int i=0; i<arr.length; i++) {	
 			for(int j=i+1; j<arr.length ; j++) {
 				if(arr[i]>arr[j]) {
-					temp =arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;			
+					temp =arr[i]; // 현재순서 숫자가 다음순서 숫자보다 크면 현재숫자를 임시로 담아둠
+					arr[i]=arr[j]; // 현재순서 숫자를 다음순서에 저장
+					arr[j]=temp;/ // 다음순서는 임시공간에 저장 따라서 오름차순으로 정렬됨		
 				}
 			}	
 		}

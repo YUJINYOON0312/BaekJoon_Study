36f215cfb3768d5c627f3bb9ede8563f39adbe02
YuJin Yoon <107790324+YUJINYOON0312@users.noreply.github.com>
2023-02-01 17:12:12 +0900

    Update and rename BackArray_1546.java to BaekArray_1546.java

--- BaekArray_1546.java
+++ BaekArray_1546.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BackArray_1546  {
+public class BaekArray_1546  {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
